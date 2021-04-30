f53cd74a4853b4000b0449092568756bd2809362
Greg Noethlich <gmnoethlich@gmail.com>
2021-04-30 00:22:19 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '815992799334545',
+        clientSecret: '7a49eff2c5eda8f3f3dccac4bf2a73b5'
+    }
 }
